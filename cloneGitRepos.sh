7bf2799180c36cd4438fb3ae09e1040956e73ce8
LucyHut <lucie.hutchins@yahoo.com>
2017-09-12 17:17:10 -0400

    Added cloneGitRepos.sh

---
+++ cloneGitRepos.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+# Organization: MDIBL
+# Author: Lucie Hutchins
+# Date: September 2017
+#
+# This script returns the release number
+
+cd `dirname $0`
+
+SCRIPT_NAME=`basename $0`
+WORKING_DIR=`pwd`
+RELEASE_NUMBER=0
+CURRENT_RELEASE_FLAG=current_release_NUMBER
+GIT=`which git`
+
+if [ ! -f $GIT ]
+then
+   echo "Git not installed on this server:"`uname -n`
+   exit 1
+fi
+
+if [ $# -lt 1 ]
+then 
+  echo "Usage: ./$SCRIPT_NAME package_name/package_name_repos.cfg"
+  echo "Example: ./$SCRIPT_NAME bedtools/bedtools_repos.cfg"
+  exit 1
+fi
+if [ ! -f Configuration ]
+then
+  echo "'Configuration' file missing "     
+  exit 1
+fi
+if [ ! -f $1 ]
+then
+  echo "'$1' file missing"     
+  exit 1
+fi
+
+source ./Configuration
+source ./$1
+
+LOG_FILE="${DOWNLOADS_LOG_DIR}/$SCRIPT_NAME.log"
+PACKAGE_INSTALL_BASE="$EXTERNAL_SOFTWARE_BASE/$SHORT_NAME"
+REPOS_URL=$GIT_CLONE_URL_BASE/$GIT_ORG/$GIT_REPOS.git
+release_flag=${PACKAGE_INSTALL_BASE}/$CURRENT_RELEASE_FLAG
+
+rm -rf $LOG_FILE
+touch $LOG_FILE
+date | tee -a $LOG_FILE
+echo "**********              *******************" | tee -a $LOG_FILE
+echo " Checking Current Release Number"| tee -a $LOG_FILE
+echo "**********  *******************************"| tee -a $LOG_FILE
+echo ""| tee -a $LOG_FILE
+echo "Path to Local install: $PACKAGE_INSTALL_BASE" | tee -a $LOG_FILE
+echo "Git repos base: $PACKAGE_GIT_CLONE_BASE" | tee -a $LOG_FILE
+echo "Organization:$GIT_ORG" | tee -a $LOG_FILE
+echo "Repos:$GIT_REPOS" | tee -a $LOG_FILE
+echo "Git url:$GIT_CLONE_URL_BASE" | tee -a $LOG_FILE
+echo "Git repos url: $REPOS_URL" | tee -a $LOG_FILE
+
+if [ ! -d $PACKAGE_GIT_CLONE_BASE ]
+then
+  mkdir -p $PACKAGE_GIT_CLONE_BASE
+fi
+
+if [ ! -d $PACKAGE_INSTALL_BASE ]
+then
+  mkdir -p $PACKAGE_INSTALL_BASE
+fi
+
+cd $PACKAGE_GIT_CLONE_BASE
+
+if [ ! -d $GIT_REPOS ]
+then
+    echo "Cloning git repos $REPOS_URL " | tee -a $LOG_FILE
+    echo "Under: "`pwd` | tee -a $LOG_FILE
+    $GIT clone $REPOS_URL 2>&1 | tee -a $LOG_FILE
+    if [ ! -d $GIT_REPOS ]
+    then
+        echo "$GIT clone $REPOS_URL FAILED"| tee -a $LOG_FILE
+        exit 1
+    fi
+fi
+cd $GIT_REPOS
+$GIT pull 2>&1 | tee -a $LOG_FILE
+RELEASE_TOKEN=`$GIT rev-list --tags --max-count=1`
+RELEASE_NUMBER=`$GIT describe --tags $RELEASE_TOKEN`
+
+## Create the current release Number file
+
+touch $release_flag
+echo "$RELEASE_NUMBER" > $release_flag
+
+echo "Current Release Number:$RELEASE_NUMBER"| tee -a $LOG_FILE
+echo ""| tee -a $LOG_FILE
+echo "Program complete"| tee -a $LOG_FILE
+
+exit 0
