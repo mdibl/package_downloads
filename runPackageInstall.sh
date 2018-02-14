bc45e9f571527d1dfcc907334685f180c2c683cd
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-02-14 12:12:19 -0500

    Update runPackageInstall.sh

--- runPackageInstall.sh
+++ runPackageInstall.sh
@@ -72,7 +72,7 @@ PACKAGE_BASE=${PACKAGE_DOWNLOADS_BASE}/${RELEASE_DIR}
 if [ -d ${PACKAGE_BASE} ]
 then
     echo " "
-    echo "***************Package Install ****************"
+    echo "************* Package Install ****************"
     echo ""
     echo "${TOOL_NAME} Version: ${RELEASE_NUMBER} is already installed"
     echo ""
