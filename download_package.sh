a42a8c132f1ed386537df960567e41a8baf8f105
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-02-15 09:41:34 -0500

    Update download_package.sh

--- download_package.sh
+++ download_package.sh
@@ -8,8 +8,8 @@
 # This creates an additional log that could be use later on
 #
 # This script is called by the runPackageInstall.sh script
-# Assumption: all the expected environment variable have been
-# sourced by the caller
+# Assumption: all the expected environment variables have been
+# sourced by the caller.
 #
 cd `dirname $0`
 WORKING_DIR=`pwd`
@@ -29,8 +29,8 @@ fi
 
 LOG=${DOWNLOADS_LOG_DIR}/${SCRIPT_NAME}.${RELEASE_DIR}.log
 WGET_COMMAND="${WGET_OPTIONS} '${REMOTE_URL}'"
-rm -f $LOG
-touch $LOG
+rm -f ${LOG}
+touch ${LOG}
 
 if [ "${DOWNLOADS_LOG_DIR}" = "" ]
 then
@@ -44,56 +44,55 @@ then
 fi
 echo "==" | tee -a ${LOG}  
 echo "Start Date:"`date` | tee -a ${LOG}  
-echo "Package: $SHORT_NAME"  | tee -a ${LOG}  
-echo "Remote site: $REMOTE_SITE"  | tee -a ${LOG}  
-echo "Remote directory: $REMOTE_DIR"  | tee -a ${LOG}  
-echo "Remote files: $REMOTE_FILES"  | tee -a ${LOG}  
-echo "Remote url: $REMOTE_URL" | tee -a ${LOG}  
+echo "Package: ${SHORT_NAME}"  | tee -a ${LOG}  
+echo "Remote site: ${REMOTE_SITE}"  | tee -a ${LOG}  
+echo "Remote directory: ${REMOTE_DIR}"  | tee -a ${LOG}  
+echo "Remote files: ${REMOTE_FILES}"  | tee -a ${LOG}  
+echo "Remote url: ${REMOTE_URL}" | tee -a ${LOG}  
 echo "==" | tee -a ${LOG}  
-echo "Local directory: $PACKAGE_DOWNLOADS_BASE" | tee -a ${LOG}  
+echo "Local directory: ${PACKAGE_DOWNLOADS_BASE}" | tee -a ${LOG}  
 echo "==" | tee -a ${LOG}  
-echo "Running $SCRIPT_NAME from: $WORKING_DIR"| tee -a ${LOG}  
+echo "Running $SCRIPT_NAME from: ${WORKING_DIR}"| tee -a ${LOG}  
 
-[ ! -d $LOCAL_DIR ] && mkdir --parents $LOCAL_DIR
-if [ $is_xml_query ]
+[ ! -d ${PACKAGE_DOWNLOADS_BASE} ] && mkdir --parents ${PACKAGE_DOWNLOADS_BASE}
+if [ ${is_xml_query} ]
 then
    ##don't do the loop if this is a xml query string
-   target_file="$REMOTE_SITE$REMOTE_DIR/${REMOTE_FILES}"
-   echo "---- $WGET $WGET_OPTIONS $target_file" | tee -a  ${LOG}  
-   cd ${LOCAL_DIR}
-   $WGET -a ${LOG} $WGET_OPTIONS "$target_file"
+   target_file="${REMOTE_URL}/${REMOTE_FILES}"
+   echo "---- ${WGET} ${WGET_OPTIONS} $target_file" | tee -a  ${LOG}  
+   cd ${PACKAGE_DOWNLOADS_BASE}
+   ${WGET} -a ${LOG} ${WGET_OPTIONS} "${target_file}"
 else
 (
 set -f
 for remote_file in ${REMOTE_FILES}
 do
-   echo "Downloading $remote_file"
-   target_file="$REMOTE_SITE$REMOTE_DIR/$remote_file"
-   echo "Downloading $target_file"
+   target_file="${REMOTE_URL}/${remote_file}"
+   echo "Downloading ${target_file}"
    ## get the directory part from the remote_file name
-   target_local_dir=`dirname $remote_file`
+   target_local_dir=`dirname ${remote_file}`
    #
    # Make parent directories as needed
-   if [ "$target_local_dir" != "" ]
+   if [ "${target_local_dir}" != "" ]
    then
-       if [ "$NO_LOCAL_PARENT_DIR" = true ]
+       if [ "${NO_LOCAL_PARENT_DIR}" = true ]
        then 
            target_local_dir=""
        else
-           echo "Creating ${LOCAL_DIR}/$target_local_dir"
-           mkdir -p ${LOCAL_DIR}/$target_local_dir
+           echo "Creating ${PACKAGE_DOWNLOADS_BASE}/${target_local_dir}"
+           mkdir -p ${PACKAGE_DOWNLOADS_BASE}/${target_local_dir}
        fi
    fi
    #cd to LOCAL_DOWNLOAD_BASE and run wget command 
-   cd ${LOCAL_DIR}/$target_local_dir 
+   cd ${PACKAGE_DOWNLOADS_BASE}/${target_local_dir} 
    # delete local files as needed
-   if [ "$do_deletes" = true ]
+   if [ "${do_deletes}" = true ]
    then
-       rm -f $remote_file
+       rm -f ${remote_file}
    fi
-   if [ "$IS_HTTP_PATTERN" = true ]
+   if [ "${IS_HTTP_PATTERN}" = true ]
    then
-       $WGET -a ${LOG} $WGET_OPTIONS -A "$remote_file" "$REMOTE_SITE$REMOTE_DIR/" 
+       ${WGET} -a ${LOG} ${WGET_OPTIONS} -A "${remote_file}" "${REMOTE_URL}/" 
     else
       echo "Running command: ${WGET} -a ${LOG} ${WGET_OPTIONS} ${target_file} "
        ${WGET}  ${WGET_OPTIONS} ${target_file} 2>&1 | tee -a ${LOG}
