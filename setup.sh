8c580cdb5a92f991d4eeb393aa3531094a6eb4eb
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-02-27 15:09:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,11 +30,11 @@ do
   [ "${script_name}" == "setup.sh" ] && continue
   chmod 755 $script_name
 done
-
+echo "  ${PACKAGE_DOWNLOADS_BASE}: is the full path to where this package is installed"
 echo "Next: Update the file 'Configuration' and set the following environment variables"
 echo "  SOFTWARE_BASE : full path to where to store external executables and libraries"
 echo "  EXTERNAL_SOFTWARE_BASE : full path to where packages will be installed "
-echo "  PACKAGE_DOWNLOADS_BASE: full path to where this package is installed"
 echo "  PACKAGE_GIT_CLONE_BASE : full path to where to clone git repos locally "
 echo "  LOGS_BASE : full path to the logs base"
 echo ""
+
